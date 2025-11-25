5a0ba33c633b9aa3d103508d4a58b6b97069877b
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2025-11-25 12:06:05 +0530

    Legal Terminus work start 🕉️

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
