b6d99186f9db1d3827e8489e5e2fdb7f184a9ba7
nibe <routnibedita516@gmail.com>
2025-11-28 23:58:35 +0530

    upto Tab bar

--- Frontend/src/Pages/PrivateLimited/PrivateLimited.jsx
+++ Frontend/src/Pages/PrivateLimited/PrivateLimited.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Breadcrum from '../../Components/Breadcrum/Breadcrum'
 import PvtltdPlanandPricing from '../../Components/PvtltdPlanandPricing/PvtltdPlanandPricing'
+import PvtltdZolvitPremium from '../../Components/PvtltdZolvitPremium/PvtltdZolvitPremium'
+import PvtltdTabs from '../../Components/PvtltdTabs/PvtltdTabs'
+import PvtltdCompany from '../../Components/PvtltdCompany/PvtltdCompany'
 
 const PrivateLimited = () => {
   return (
     <div>
         <Breadcrum/>
         <PvtltdPlanandPricing />
+        <PvtltdZolvitPremium />
+        <PvtltdTabs />
+        <PvtltdCompany />
     </div>
   )
 }
