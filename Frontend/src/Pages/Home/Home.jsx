d819c330e843893a01ba87e2690d2c52972572a1
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2025-12-23 17:53:27 +0530

    Hero Sec update

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import './Home.css'
 import Herosection from '../../Components/Herosection/Herosection'
 import Premiumbusiness from '../../Components/Premiumbusiness/Premiumbusiness'
-import Compilance from '../../Components/Compilance/Compilance'
 import Workingprocess from '../../Components/WorkingProcessPro/WorkingProcessPro'
 import Whoweare from '../../Components/Whoweare/Whoweare'
 import Featureslegalservice from '../../Components/Featureslegalservice/Featureslegalservice'
 import Legalhelp from '../../Components/Legalhelp/Legalhelp'
+import HomeCertisfiedClient from '../../Components/HomeCertisfiedClient/HomeCertisfiedClient'
 
 const Home = () => {
   return (
     <>
-        <Herosection/>
+        {/* <Herosection/> */}
         <Premiumbusiness/>
-        <Compilance/>
         <Workingprocess/>
         <Legalhelp/>
+        <HomeCertisfiedClient />
         <Whoweare/>
         <Featureslegalservice/>
     </>
