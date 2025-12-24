cb9d0d4d08610367ea532cefe7291a164dfcb5af
nibe <routnibedita516@gmail.com>
2025-12-24 14:52:41 +0530

    pull

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Home.css'
 import Herosection from '../../Components/Herosection/Herosection'
 import Premiumbusiness from '../../Components/Premiumbusiness/Premiumbusiness'
-import Compilance from '../../Components/Compilance/Compilance'
+// import Compilance from '../../Components/Compilance/Compilance'
 import Workingprocess from '../../Components/WorkingProcessPro/WorkingProcessPro'
 import Whoweare from '../../Components/Whoweare/Whoweare'
 import Featureslegalservice from '../../Components/Featureslegalservice/Featureslegalservice'
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <>
         <Herosection/>
-        <Compilance/>
+        {/* <Compilance/>../ */}
         <Premiumbusiness/>
         <Workingprocess/>
         <Legalhelp/>
