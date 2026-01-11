b6a9a4adbbc2963f417d518d668e0ad41423912c
SarojKumarMallik <sarojkumarmallik94@gmail.com>
2026-01-12 00:15:07 +0530

    Testimonial

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,35 @@
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
 
-import app from "./app.js";
+import blogRoutes from "./routes/blog.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
+import testimonialRoutes from "./routes/Testimonial.routes.js";
 import { connectDB } from "./config/db.js";
 
-const PORT = process.env.PORT || 5000;
+/* ================= LOAD ENV ================= */
+dotenv.config();
 
+/* ================= INIT APP ================= */
+const app = express();
+
+/* ================= MIDDLEWARE ================= */
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+/* ================= ROUTES ================= */
+app.use("/api/admin/blog", blogRoutes);
+app.use("/api/admin/category", categoryRoutes);
+app.use("/api/testimonials", testimonialRoutes);
+
+
+/* ================= DB CONNECT ================= */
 connectDB();
 
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+/* ================= SERVER ================= */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
