fbb6aaac65f0581556a79354a43e306aaf066b1e
SarojKumarMallik <sarojkumarmallik94@gmail.com>
2026-01-12 10:25:08 +0530

    Testimonial Section

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,43 +6,43 @@ import blogRoutes from "./routes/blog.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import testimonialRoutes from "./routes/testimonial.routes.js";
 import employeeRoutes from "./routes/employee.routes.js";
+import videoTestimonialRoutes from "./routes/videotestimonial.routes.js";
+import clientRoutes from "./routes/client.routes.js";
+
 import { connectDB } from "./config/db.js";
 
-/* ================= LOAD ENV ================= */
 dotenv.config();
 
-/* ================= INIT APP ================= */
 const app = express();
 
-/* ================= MIDDLEWARE ================= */
+/* ✅ CORS FIX */
 app.use(
   cors({
-    origin: "http://localhost:5175", // frontend
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
 
-/* ================= ROUTES ================= */
 app.use("/api/admin/blog", blogRoutes);
 app.use("/api/admin/category", categoryRoutes);
 app.use("/api/testimonials", testimonialRoutes);
 app.use("/api/employees", employeeRoutes);
+app.use("/api/video-testimonials", videoTestimonialRoutes);
 
-/* ================= START SERVER ================= */
+
+app.use("/api/clients", clientRoutes);
+
+
+/* START SERVER */
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("❌ Server start failed:", error);
-  }
+  await connectDB();
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  );
 };
 
 startServer();
